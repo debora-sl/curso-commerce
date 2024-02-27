07fcb427692682ffb9a4e5ab802a626f76c71e64
Debora Souza <debora.souzalima22@gmail.com>
2024-02-27 18:27:15 -0300

    Criação do componente Sidebar. Inclusão do Sidebar e h1 Main para visualização inicial.

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,12 @@
+
 import { Container } from './styles'
+import { Sidebar } from '../../components/Sidebar'
 
 export default function Main() {
   return (
     <Container>
-
+      <Sidebar />
+      <h1>Main</h1>
     </Container>
   )
 }
