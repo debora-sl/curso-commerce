258d56224902cfd9279aa249bbc13f36a0fc1e2c
Debora Souza <debora.souzalima22@gmail.com>
2024-03-01 06:55:46 -0300

    Estilização do Sidebar para mobiles.

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -102,4 +102,58 @@ export const Container = styled.aside<ContainerProps>`
             }
         }
     }
+
+  //estilização para mobiles
+  @media (max-width: 720px) {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom:0;
+    z-index: 999;
+
+    width: 100%;
+    height: 5rem;
+    //barra de scroll
+    overflow-y: auto;
+    padding: 0 0;
+
+    //escondendo o botão
+    button{
+      display: none;
+    }
+
+    nav{
+      height: 100%;
+
+      ul{
+        flex-direction: row;
+        align-items: center;
+      }
+
+      li {
+        a {
+          flex-direction: column;
+          padding: 0rem;
+
+          svg {
+            width: 3.25rem;
+            height: 3.25rem;
+          }
+
+          //escondendo os titulos
+          span{
+            display: none;
+          }
+
+          &.active {
+            &::after {
+              display: none;
+            }
+          }
+        }
+      }
+    }
+
+
+  }
 `
