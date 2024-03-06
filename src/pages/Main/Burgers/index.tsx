1b3d777634f37cf30a9f34db39606474584928c5
Debora Souza <debora.souzalima22@gmail.com>
2024-03-06 12:51:15 -0300

    Criação do array fixo de dados.

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -3,7 +3,27 @@ import { SnackTitle } from '../../../components/SnackTitle'
 
 //incluir página como export default
 export default function Burgers() {
+  const data = [
+    {
+      id: 1,
+      snack: 'burger',
+      name: 'Mega',
+      description: 'O artesanal tamanho familia com três carnes suculentas, queijo e bacon.',
+      price: 25.50,
+      image: 'https://i.imgur.com/upjIUnG.jpg'
+    },
+    {
+      id: 2,
+      snack: 'burger',
+      name: 'Extra bacon',
+      description: 'Criado para os amantes de bacon, possui em todas as suas camadas bacon bem assado e ainda queijo e carne.',
+      price: 23.50,
+      image: 'https://i.imgur.com/B4J04AJ.jpg'
+    },
+  ]
+
   return(
+    //criando o array fixo de dados
     <>
       {/* chamando o componente Head com a propriedade title e informando o conteúdo */}
       <Head title='Hambúrgueres' />
