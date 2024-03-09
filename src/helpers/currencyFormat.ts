16d104c176c3d13e2afb47f411e89076d4858377
Debora Souza <debora.souzalima22@gmail.com>
2024-03-09 17:52:39 -0300

    Criação do componente helpers para utilização da função format, para formatação de moedas.

---
+++ src/helpers/currencyFormat.ts
@@ -0,0 +1,7 @@
+//função para formatação de moedas
+export function currencyFormat(currency: number) {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(currency)
+}
