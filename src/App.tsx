792b488c874e94c7a8db9d000fa398443b74cbfb
Debora Souza <debora.souzalima22@gmail.com>
2024-03-12 20:43:18 -0300

    Inclusão do CartProvider abaixo do SnackProvider e do ToastContainer para o usuário visualizar quando adicionar algum snack.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
+import { SnackProvider } from './contexts/SnackContext'
+import { CartProvider } from './contexts/CartContext'
 import { AppRoutes } from './routes'
 
 import { Theme } from './styles/Theme'
 import { GlobalStyle } from './styles/global'
 import { Normalize } from 'styled-normalize'
-import { SnackProvider } from './contexts/SnackContext'
+
 
 export default function App() {
 
@@ -14,9 +18,12 @@ export default function App() {
     <BrowserRouter>
       <Theme>
         <SnackProvider>
-          <AppRoutes />
-          <GlobalStyle />
-          <Normalize />
+          <CartProvider>
+            <AppRoutes />
+            <ToastContainer autoClose={2000}/>
+            <GlobalStyle />
+            <Normalize />
+          </CartProvider>
         </SnackProvider>
       </Theme>
     </BrowserRouter>
