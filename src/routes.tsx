bcc156f83c5f796929e097fc007f220fb99c7d5b
Debora Souza <debora.souzalima22@gmail.com>
2024-03-13 21:27:48 -0300

    Inclusão da rota de MyCart (Meu Carrinho) em routes.

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,6 +6,7 @@ import BurgersPage from './pages/Main/Burgers'
 import PizzasPage from './pages/Main/Pizzas'
 import DrinksPage from './pages/Main/Drinks'
 import IceCreamsPage from './pages/Main/IceCreams'
+import MyCartPage from './pages/MyCart'
 
 export function AppRoutes() {
   return (
@@ -16,6 +17,7 @@ export function AppRoutes() {
         <Route path='drinks' element={<DrinksPage />} />
         <Route path='ice-creams' element={<IceCreamsPage />} />
       </Route>
+      <Route path='/cart' element={<MyCartPage />} />
     </Routes>
   )
 }
