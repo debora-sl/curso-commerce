7960b30ad82317e6252f2ffd585d1156e9a246e6
Debora Souza <debora.souzalima22@gmail.com>
2024-03-16 13:28:46 -0300

    Criação e Estilização do componente EmptyCart (Carrinho vázio), que verifica se o carrinho está vazio e disponibiliza o botão que direciona o usuário para o cardápio.

---
+++ src/components/EmptyCart/index.tsx
@@ -0,0 +1,17 @@
+import { Container, Button } from './styles'
+
+import manAndBurgerImg from '../../assets/man-and-burger.svg'
+
+interface EmptyCartProps {
+  title: string
+}
+
+export function EmptyCart({title}: EmptyCartProps){
+  return(
+    <Container>
+      <h2>{title}</h2>
+      <Button to='/'>Checar cardápio</Button>
+      <img src={manAndBurgerImg} alt='Homem com hambúrguer' />
+    </Container>
+  )
+}
