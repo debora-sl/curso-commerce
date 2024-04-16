36d59cf13c8969b2ec461e3c593375f6395c5bd4
Debora Souza <debora.souzalima22@gmail.com>
2024-04-16 06:59:22 -0300

    Validação dos campos CPF/CNPJ e Endereço.

--- src/pages/Payment/index.tsx
+++ src/pages/Payment/index.tsx
@@ -81,7 +81,22 @@ export default function Payment(){
 
             <div className='field'>
               <label htmlFor="document">CPF/ CNPJ</label>
-              <input type="text" name="document" id="document" />
+
+              <Controller
+                name='document'
+                control={control}
+                render={({ field }) => (
+                  <IMaskInput
+                    type='text'
+                    id='document'
+                    mask={[
+                      { mask: '000.000.000-00', maxLength: 11 },
+                      { mask: '00.000.000/0000-00' },
+                    ]}
+                    {...field}
+                  />
+                )}
+              />
             </div>
           </div>
 
@@ -89,70 +104,115 @@ export default function Payment(){
 
           <div className='field'>
             <label htmlFor="zipcode">CEP</label>
-            <input type="text" name="zipcode" id="zipcode" autoComplete='postal-code' style={ {width: '120px'} }/>
+            <Controller
+              name='zipCode'
+              control={control}
+              render={({ field }) => (
+                <IMaskInput
+                  type='text'
+                  id='zipCode'
+                  style={{ width: '120px' }}
+                  mask={'00000-000'}
+                  {...field}
+                />
+              )}
+            />
+            {errors.zipCode && <p className='error'>{errors.zipCode.message}</p>}
           </div>
 
           <div className='field'>
             <label htmlFor="street">Endereço</label>
-            <input type="text" name="street" id="street" autoComplete='street-address'/>
+            <Controller
+              name='street'
+              control={control}
+              render={({ field }) => <input type='text' id='street' {...field} />}
+            />
+            {errors.street && <p className='error'>{errors.street.message}</p>}
           </div>
 
           <div className='grouped'>
             <div className='field'>
              <label htmlFor="number">Número</label>
-             <input type="text" name="number" id="number" />
+             <Controller
+                name='number'
+                control={control}
+                render={({ field }) => <input type='text' id='number' {...field} />}
+              />
+              {errors.number && <p className='error'>{errors.number.message}</p>}
             </div>
 
 
             <div className='field'>
              <label htmlFor="complement">Complemento</label>
-             <input type="text" name="complement" id="complement" />
+             <Controller
+                name='complement'
+                control={control}
+                render={({ field }) => <input type='text' id='complement' {...field} />}
+              />
+              {errors.complement && <p className='error'>{errors.complement.message}</p>}
             </div>
           </div>
 
           <div className='grouped'>
             <div className='field'>
              <label htmlFor="neighborhood">Bairro</label>
-             <input type="text" name="neighborhood" id="neighborhood" />
+             <Controller
+                name='neighborhood'
+                control={control}
+                render={({ field }) => <input type='text' id='neighborhood' {...field} />}
+              />
+              {errors.neighborhood && <p className='error'>{errors.neighborhood.message}</p>}
             </div>
 
             <div className='field'>
              <label htmlFor="city">Cidade</label>
-             <input type="text" name="city" id="city" />
+             <Controller
+                name='city'
+                control={control}
+                render={({ field }) => <input type='text' id='city' {...field} />}
+              />
+              {errors.city && <p className='error'>{errors.city.message}</p>}
             </div>
 
             <div className='field'>
              <label htmlFor="state">Estado</label>
-             <select name="state" id="state">
-             <option value=''>Selecione</option>
-                <option value='AC'>Acre</option>
-                <option value='AL'>Alagoas</option>
-                <option value='AP'>Amapá</option>
-                <option value='AM'>Amazonas</option>
-                <option value='BA'>Bahia</option>
-                <option value='CE'>Ceará</option>
-                <option value='ES'>Espírito Santo</option>
-                <option value='GO'>Goiás</option>
-                <option value='MA'>Maranhão</option>
-                <option value='MT'>Mato Grosso</option>
-                <option value='MS'>Mato Grosso do Sul</option>
-                <option value='MG'>Minas Gerais</option>
-                <option value='PA'>Pará</option>
-                <option value='PB'>Paraíba</option>
-                <option value='PR'>Paraná</option>
-                <option value='PE'>Pernambuco</option>
-                <option value='PI'>Piauí</option>
-                <option value='RJ'>Rio de Janeiro</option>
-                <option value='RN'>Rio Grande do Norte</option>
-                <option value='RS'>Rio Grande do Sul</option>
-                <option value='RO'>Rondônia</option>
-                <option value='RR'>Roraima</option>
-                <option value='SC'>Santa Catarina</option>
-                <option value='SP'>São Paulo</option>
-                <option value='SE'>Sergipe</option>
-                <option value='TO'>Tocantins</option>
-                <option value='DF'>Distrito Federal</option>
-             </select>
+             <Controller
+                name='state'
+                control={control}
+                render={({ field }) => (
+                  <select id='state' {...field}>
+                    <option value=''>Selecione</option>
+                    <option value='AC'>Acre</option>
+                    <option value='AL'>Alagoas</option>
+                    <option value='AP'>Amapá</option>
+                    <option value='AM'>Amazonas</option>
+                    <option value='BA'>Bahia</option>
+                    <option value='CE'>Ceará</option>
+                    <option value='ES'>Espírito Santo</option>
+                    <option value='GO'>Goiás</option>
+                    <option value='MA'>Maranhão</option>
+                    <option value='MT'>Mato Grosso</option>
+                    <option value='MS'>Mato Grosso do Sul</option>
+                    <option value='MG'>Minas Gerais</option>
+                    <option value='PA'>Pará</option>
+                    <option value='PB'>Paraíba</option>
+                    <option value='PR'>Paraná</option>
+                    <option value='PE'>Pernambuco</option>
+                    <option value='PI'>Piauí</option>
+                    <option value='RJ'>Rio de Janeiro</option>
+                    <option value='RN'>Rio Grande do Norte</option>
+                    <option value='RS'>Rio Grande do Sul</option>
+                    <option value='RO'>Rondônia</option>
+                    <option value='RR'>Roraima</option>
+                    <option value='SC'>Santa Catarina</option>
+                    <option value='SP'>São Paulo</option>
+                    <option value='SE'>Sergipe</option>
+                    <option value='TO'>Tocantins</option>
+                    <option value='DF'>Distrito Federal</option>
+                  </select>
+                )}
+              />
+              {errors.state && <p className='error'>{errors.state.message}</p>}
             </div>
           </div>
 
--- src/pages/Payment/validationSchema.tsx
+++ src/pages/Payment/validationSchema.tsx
@@ -8,12 +8,31 @@ export const schema = yup
       .required('O nome é obrigatório.')
       .min(3, 'O nome deve ser completo.')
       .matches(/(\w.+\s).+/gi, 'O nome deve conter o sobrenome.'),
-    email: yup.string().required('O email é obrigatório.').email('O email deve ser válido, por favor verificar.'),
+    email: yup.string().required('O email é obrigatório.').email('O email deve ser válido.'),
     mobile: yup
     .string()
     .required('O celular é obrigatório.')
     .transform((value) => value.replace(/[^\d]/g, ''))
-    .test('validateMobile', 'O celular preenchido inválido.', (value) => isValidPhone(value)),
+    .test('validateMobile', 'O celular inválido.', (value) => isValidPhone(value)),
+    document: yup
+      .string()
+      .required('O CPF/CNPJ é obrigatório.')
+      .transform((value) => value.replace(/[^\d]/g, ''))
+      .test(
+        'validateDocument',
+        'O CPF/CNPJ é inválido.',
+        (value) => isValidCPF(value) || isValidCNPJ(value),
+      ),
+    zipCode: yup
+      .string()
+      .required('O CEP é obrigatório.')
+      .transform((val) => val.replace(/[^\d]+/g, '')),
+      street: yup.string().required('O endereço é obrigatório.'),
+      number: yup.string().required('O número é obrigatório.'),
+      complement: yup.string(),
+      neighborhood: yup.string().required('O bairro é obrigatório.'),
+      city: yup.string().required('A cidade é obrigatória.'),
+      state: yup.string().required('O estado é obrigatório.'),
   })
   .required()
 
