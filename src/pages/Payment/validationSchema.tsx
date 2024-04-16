7b263227f5c31a3d5c095caafd4ad8acde3d7522
Debora Souza <debora.souzalima22@gmail.com>
2024-04-15 22:59:37 -0300

    Ajuste na mensagem de erro do campo e-mail.

--- src/pages/Payment/validationSchema.tsx
+++ src/pages/Payment/validationSchema.tsx
@@ -8,7 +8,7 @@ export const schema = yup
       .required('O nome é obrigatório.')
       .min(3, 'O nome deve ser completo.')
       .matches(/(\w.+\s).+/gi, 'O nome deve conter o sobrenome.'),
-    email: yup.string().required('O email é obrigatório.').email('O email deve ser válido.'),
+    email: yup.string().required('O email é obrigatório.').email('O email deve ser válido, por favor verificar.'),
     mobile: yup
     .string()
     .required('O celular é obrigatório.')
